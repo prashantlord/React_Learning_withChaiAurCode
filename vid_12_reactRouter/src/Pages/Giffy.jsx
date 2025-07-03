6e1e41ebe347b3814b6335f1944e7ce03e0d86b1
Prashant Shrestha <prashantstha0077@gmail.com>
2025-07-03 12:21:56 +0545

    Update Giffy.jsx

--- vid_12_reactRouter/src/Pages/Giffy.jsx
+++ vid_12_reactRouter/src/Pages/Giffy.jsx
@@ -1,19 +1,6 @@
 import React, { useEffect, useState } from "react";
 
 function Giffy() {
-  const [fetchData, setFetchData] = useState({});
-
-  useEffect(() => {
-    fetch(
-      "https://api.giphy.com/v1/gifs/random?api_key=OUr59fWLBSz8krYhgJPqSG17q9o6Pv3f&tag=&rating=g"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setFetchData(data);
-        console.log(fetchData);
-      });
-  }, [fetchData]);
-
   return (
     <>
       <div className="w-full h-100 flex items-center">
